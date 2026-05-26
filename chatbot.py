7a409dd6eb929701fe75d214148242c574d24a11
Soban AKbar <mu.sobanakbar@gmail.com>
2026-05-26 04:13:15 +0400

    fixed hardcoding model name, and max tokens

--- chatbot.py
+++ chatbot.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 client = Groq()
-
-
+MODEL_NAME = "openai/gpt-oss-120b"
+MAX_TOKEN_LIMIT = 1024
 
 messages= [
     {"role": "system", "content": "you don't respond directly to anything the user says, you just give a fact you haven't gave before"}
@@ -18,8 +18,8 @@ while True:
     user_input = input("You: ")
     messages.append({"role":"user", "content":user_input})
     message = client.chat.completions.create(
-        model = "openai/gpt-oss-120b",
-        max_completion_tokens = 1024,
+        model = MODEL_NAME,
+        max_completion_tokens = MAX_TOKEN_LIMIT,
         messages = messages,
         temperature = 0.7,
          
